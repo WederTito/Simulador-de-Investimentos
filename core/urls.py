b8cb8a1b8078da8ca717473e7cb8b5fe77b0cb88
wedertito <wedertito8@gmail.com>
2024-08-22 14:58:21 -0300

    filtros + rotas navbar

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,5 +9,6 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('empresarios/', include('empresarios.urls')),
     path('investidores/', include('investidores.urls')),
+    path('', lambda request: redirect ('/empresarios/cadastrar_empresa'))
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
